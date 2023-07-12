b89db7ef1defcbfef641640fb40c306097f706d2
feaxmutch <feaxmutch@gmail.com>
2023-07-12 14:41:13 +0300

    12.07.2023.14:41

---
+++ IJ_homeWork_localMax/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace IJ_homeWork_localMax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int minNumber = 0;
+            int maxNumber = 9;
+            int[] numbers = new int[30];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(minNumber, maxNumber + 1);
+                Console.Write($"{numbers[i]} ");
+            }
+
+            Console.WriteLine("\n\nВсе локальные максимумы:");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i == 0)
+                {
+                    if (numbers[i] > numbers[i + 1])
+                    {
+                        Console.Write($"{numbers[i]} ");
+                    }
+                }
+                else if (i == numbers.Length - 1)
+                {
+                    if (numbers[i] > numbers[i - 1])
+                    {
+                        Console.Write($"{numbers[i]} ");
+                    }
+                }
+                else
+                {
+                    if (numbers[i] > numbers[i - 1] && numbers[i] > numbers[i + 1])
+                    {
+                        Console.Write($"{numbers[i]} ");
+                    }
+                }
+            }
+        }
+    }
+}

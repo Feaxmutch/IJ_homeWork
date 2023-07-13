4f709bd9bb0df3a9487b8bf6321bf8554b3a719e
feaxmutch <feaxmutch@gmail.com>
2023-07-13 16:28:46 +0300

    13.07.2023.16:28

---
+++ IJ_homeWork_wordToNewLine/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace IJ_homeWork_wordToNewLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fontPangram = "Съешь ещё этих мягких французских булок, да выпей чаю.";
+            string[] words = fontPangram.Split(' ');
+
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}

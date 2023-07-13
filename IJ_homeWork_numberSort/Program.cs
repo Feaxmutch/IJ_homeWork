004d607d9b3fa0b3456017f2adb9f4610354cd3d
feaxmutch <feaxmutch@gmail.com>
2023-07-13 16:08:21 +0300

    13.07.2023.16:08

---
+++ IJ_homeWork_numberSort/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace IJ_homeWork_numberSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int minNumber = 0;
+            int maxNumber = 9;
+            int[] numbers = new int[20];
+
+            Console.WriteLine("Числа без сортировки:");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(minNumber, maxNumber + 1);
+                Console.Write($"{numbers[i]} ");
+            }
+
+            Console.WriteLine("\n\n" + "Числа отсортированые по возрастанию:");
+
+            for (int i = minNumber; i <= maxNumber; i++)
+            {
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if (numbers[j] == i)
+                    {
+                        Console.Write($"{numbers[j]} ");
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

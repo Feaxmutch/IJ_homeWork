64fbca04e4ff1325234873e9dc6c9788a5c8d432
feaxmutch <feaxmutch@gmail.com>
2023-07-14 15:27:17 +0300

    14.07.2023.15:27

--- IJ_homeWork_numberSort/Program.cs
+++ IJ_homeWork_numberSort/Program.cs
@@ -8,6 +8,7 @@
             int minNumber = 0;
             int maxNumber = 9;
             int[] numbers = new int[20];
+            int numberBuffer = 0;
 
             Console.WriteLine("Числа без сортировки:");
 
@@ -19,15 +20,32 @@
 
             Console.WriteLine("\n\n" + "Числа отсортированые по возрастанию:");
 
-            for (int i = minNumber; i <= maxNumber; i++)
+            for (int i = 0; i < numbers.Length / 2; i++)
             {
-                for (int j = 0; j < numbers.Length; j++)
+                for (int j = 0; j < numbers.Length - 1; j++)
                 {
-                    if (numbers[j] == i)
+                    if (numbers[j] > numbers[j + 1])
                     {
-                        Console.Write($"{numbers[j]} ");
+                        numberBuffer = numbers[j + 1];
+                        numbers[j + 1] = numbers[j];
+                        numbers[j] = numberBuffer;
                     }
                 }
+
+                for (int j = numbers.Length - 1; j > 0; j--)
+                {
+                    if (numbers[j] < numbers[j - 1])
+                    {
+                        numberBuffer = numbers[j - 1];
+                        numbers[j - 1] = numbers[j];
+                        numbers[j] = numberBuffer;
+                    }
+                }
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"{numbers[i]} ");
             }
 
             Console.ReadKey();

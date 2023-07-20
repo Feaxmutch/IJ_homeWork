a6d76263df4a92e315cc76f76cd127df81904079
feaxmutch <feaxmutch@gmail.com>
2023-07-20 15:32:46 +0300

    20.07.2023.15:32

---
+++ Ij_homeWork_readInt/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Ij_homeWork_readInt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadNumber();
+        }
+
+        static void ReadNumber()
+        {
+            bool parseIsSuccessful = false;
+            string userInput = string.Empty;
+            int result;
+
+            while (parseIsSuccessful == false)
+            {
+                Console.Clear();
+                Console.Write("Введите число: ");
+                userInput = Console.ReadLine();
+
+                if (int.TryParse(userInput, out result))
+                {
+                    parseIsSuccessful = true;
+                    result = int.Parse(userInput);
+                    Console.WriteLine(result);
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Похоже вы ввели не число.");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}

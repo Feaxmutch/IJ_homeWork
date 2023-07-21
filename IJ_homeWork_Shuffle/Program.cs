4959ce198a73fefa1d55ee1d3218f4c51fe0e6c9
feaxmutch <feaxmutch@gmail.com>
2023-07-21 13:28:08 +0300

    21.07.2023.13:28

---
+++ IJ_homeWork_Shuffle/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace IJ_homeWork_Shuffle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = GenerateArray(10, 0, 9);
+
+            Console.WriteLine("Сгенерированый массив:");
+
+            foreach (int number in array)
+            {
+                Console.Write($"{number} ");
+            }
+
+            array = Shuffle(array, 20);
+            Console.WriteLine("\n\n" + "Перемешанный массив:");
+
+            foreach (int number in array)
+            {
+                Console.Write($"{number} ");
+            }
+
+            Console.ReadKey();
+        }
+
+        static int[] GenerateArray(int lenght, int minNumber, int maxNumber)
+        {
+            int[] array = new int[lenght];
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(minNumber, maxNumber + 1);
+            }
+
+            return array;
+        }
+
+        static int[] Shuffle(int[] array, int swipeTimes)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < swipeTimes; i++)
+            {
+                array = SwipeElements(array, random.Next(0, array.Length), random.Next(0, array.Length));
+            }
+
+            return array;
+        }
+
+        static int[] SwipeElements(int[] array, int firstIndex, int secondIndex)
+        {
+            int numberBuffer;
+
+            if (firstIndex == secondIndex)
+            {
+                if (secondIndex < array.Length - 1)
+                {
+                    secondIndex = firstIndex + 1;
+                }
+                else
+                {
+                    secondIndex = firstIndex - 1;
+                }
+            }
+
+            numberBuffer = array[firstIndex];
+            array[firstIndex] = array[secondIndex];
+            array[secondIndex] = numberBuffer;
+
+            return array;
+        }
+    }
+}
---
+++ IJ_homeWork_symbolMap/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace IJ_homeWork_symbolMap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isWorking = true;
+            char character = Convert.ToChar(File.ReadAllText("character.txt"));
+            int[] characterCordinates = new int[2];
+            characterCordinates[0] = 5;
+            characterCordinates[1] = 5;
+            char[,] map = CreateMapFromFile("map.txt");
+
+            Console.CursorVisible = false;
+            
+            while (isWorking)
+            {
+                Update(map, character, characterCordinates, ref isWorking);
+            }
+        }
+
+        static void Update(char[,] map, char character, int[] characterCordinates, ref bool ContinueСondition)
+        {
+            WriteMap(map);
+            WriteCharacter(character, characterCordinates);
+        }
+
+        static void WriteMap(char[,] map)
+        {
+            Console.SetCursorPosition(0, 0);
+
+            for (int i = 0; i < map.GetLength(1); i++)
+            {
+                for (int j = 0; j < map.GetLength(0); j++)
+                {
+                    Console.Write(map[j,i]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static char[,] CreateMapFromFile(string filePath)
+        {
+            string[] file = File.ReadAllLines(filePath);
+            char[,] map = new char[file[0].Length, file.Length];
+
+            for (int i = 0; i < map.GetLength(1); i++)
+            {
+                for (int j = 0; j < map.GetLength(0); j++)
+                {
+                    map[j, i] = file[i][j];
+                }
+            }
+
+            return map;
+        }
+
+        static void WriteCharacter(char symbol, int[] cordinate)
+        {
+            Console.SetCursorPosition(cordinate[0], cordinate[1]);
+            Console.Write(symbol);
+        }
+    }
+}

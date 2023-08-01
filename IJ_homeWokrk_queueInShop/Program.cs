f8e2bf4b16d541cd1cdb6e1781416342f6f84056
feaxmutch <feaxmutch@gmail.com>
2023-08-01 16:43:25 +0300

    01.08.2023.16:43

--- IJ_homeWokrk_queueInShop/Program.cs
+++ IJ_homeWokrk_queueInShop/Program.cs
@@ -5,32 +5,26 @@
         static void Main(string[] args)
         {
             Random random = new Random();
-            int minNumber = 10;
-            int maxNumber = 1000;
-            int minQueueLength = 3;
-            int maxQueueLength = 10;
+            
             int shopCashe = 0;
             Queue<int> customers = new Queue<int>();
 
-            GenerateQueue(customers ,random.Next(minQueueLength, maxQueueLength + 1), minNumber, maxNumber);
-
-            while (customers.Count > 0)
-            {
-                Console.Clear();
-                WriteShopStatus(shopCashe, customers.Count);
-                ServeCustomer(customers, ref shopCashe);
-                Console.ReadKey(true);
-            }
-
+            GenerateQueue(customers);
+            shopCashe = ServeCustomers(customers, shopCashe);
             Console.Clear();
             WriteShopStatus(shopCashe, customers.Count);
             Console.WriteLine("Все клиенты обслужены.");
             Console.ReadKey(true);
         }
 
-        static void GenerateQueue(Queue<int> queueColetion, int length, int minNumber, int maxNumber)
+        static void GenerateQueue(Queue<int> queueColetion)
         {
             Random random = new Random();
+            int minNumber = 10;
+            int maxNumber = 1000;
+            int minRandomLength = 3;
+            int maxRandomLength = 10;
+            int length = random.Next(minRandomLength, maxRandomLength + 1);
 
             for (int i = 0; i < length; i++)
             {
@@ -38,11 +32,18 @@
             }
         }
 
-        static void ServeCustomer(Queue<int> customersQueue, ref int shopCashe)
+        static int ServeCustomers(Queue<int> customersQueue, int shopCashe)
         {
-            shopCashe += customersQueue.Peek();
-            Console.WriteLine($"Нынешний клиент совершил покупку в размере: {customersQueue.Peek()}");
-            customersQueue.Dequeue();
+            while (customersQueue.Count > 0)
+            {
+                Console.Clear();
+                WriteShopStatus(shopCashe, customersQueue.Count);
+                Console.WriteLine($"Нынешний клиент совершил покупку в размере: {customersQueue.Peek()}");
+                shopCashe += customersQueue.Dequeue();
+                Console.ReadKey(true);
+            }
+
+            return shopCashe;
         }
 
         static void WriteShopStatus(int shopCashe, int customersCount)

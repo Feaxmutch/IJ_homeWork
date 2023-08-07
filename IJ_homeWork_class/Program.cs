fa9e9b60940b3c960e4df5943c0cb55856051c1d
feaxmutch <feaxmutch@gmail.com>
2023-08-07 15:59:35 +0300

    07.08.2023.16:00

---
+++ IJ_homeWork_class/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace IJ_homeWork_class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string CommandEscape = "Escape";
+
+            bool isWorking = true;
+            string userInput;
+            Player player = new Player("Marakashka");
+
+            while (isWorking)
+            {
+                Console.Clear();
+                player.ShowInfo();
+
+                Console.WriteLine($"Нажмите любую клавишу, чтобы поднять уровень, или {CommandEscape}, чтобы закрыть программу.");
+                userInput = Console.ReadKey().Key.ToString();
+
+                if (userInput == CommandEscape)
+                {
+                    isWorking = false;
+                }
+
+                player.UpLevel();
+            }
+        }
+    }
+
+    class Player
+    {
+        private string _Name;
+        private int _Level;
+
+        public Player(string name)
+        {
+            _Name = name;
+            _Level = 0;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Имя: {_Name}\nУровень: {_Level}");
+        }
+
+        public void UpLevel()
+        {
+            _Level++;
+        }
+    }
+}

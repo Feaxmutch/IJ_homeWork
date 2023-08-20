7cd129ff7501213c1e5e7b2314af83bdf03214e4
feaxmutch <feaxmutch@gmail.com>
2023-08-20 11:18:50 +0300

    20.08.2023.11:18

--- oop/IJ_homeWork_shop/Program.cs
+++ oop/IJ_homeWork_shop/Program.cs
@@ -42,17 +42,62 @@
         }
     }
 
-    class Seller
+    class Human
     {
-        private List<Item> _products = new List<Item>();
+        protected List<Item> _items = new List<Item>();
 
+        public int Money { get; protected set; }
+
+        public void ShowInfo()
+        {
+            Console.Clear();
+            ShowMoney();
+            Console.WriteLine();
+            ShowItems();
+            Console.ReadKey();
+        }
+
+        public int TakeMoney(int money)
+        {
+            if (money <= Money)
+            {
+                return Money -= money;
+            }
+
+            return 0;
+        }
+
+        public void GetItem(Item item)
+        {
+            if (item != null)
+            {
+                _items.Add(item);
+            }
+        }
+
+        protected virtual void ShowItems()
+        {
+            foreach (var item in _items)
+            {
+                item.ShowInfo();
+            }
+        }
+
+        private void ShowMoney()
+        {
+            Console.WriteLine($"Монеты: {Money}");
+        }
+    }
+
+    class Seller : Human
+    {
         public Seller(List<Item> products, int count)
         {
             foreach (Item product in products)
             {
                 for (int i = 0; i < count; i++)
                 {
-                    _products.Add(product);
+                    _items.Add(product);
                 }
             }
         }
@@ -77,7 +122,7 @@
                 switch (userInput)
                 {
                     case MenuProducts:
-                        ShowProducts();
+                        ShowItems();
                         break;
 
                     case MenuBuy:
@@ -91,20 +136,20 @@
             }
         }
 
-        private void ShowProducts()
+        protected override void ShowItems()
         {
             Console.Clear();
 
-            for (int i = 0; i < _products.Count; i++)
+            for (int i = 0; i < _items.Count; i++)
             {
                 Console.Write(i + 1 + ". ");
-                _products[i].ShowInfo();
+                _items[i].ShowInfo();
             }
 
             Console.ReadKey();
         }
 
-        private Item TakeProduct(Item product, int money)
+        private Item TakeItem(Item product, int money)
         {
             if (money >= product.Price)
             {
@@ -114,26 +159,41 @@
             return null;
         }
 
-        private Item GetProduct(int productNumber)
+        private Item GetItemByNumber(int productNumber)
         {
-            return _products[productNumber - 1];
+            if (productNumber > 0 && productNumber <= _items.Count)
+            {
+                return _items[productNumber - 1];
+            }
+            else
+            {
+                return null;
+            }
         }
 
         private void Buy(Player player)
         {
             Console.Clear();
-            ShowProducts();
+            ShowItems();
 
             Console.WriteLine("Введите номер предмета, который хотите купить:");
             string userInput = Console.ReadLine();
 
             if (int.TryParse(userInput, out int number))
             {
-                Item itemForBuy = GetProduct(number);
+                Item itemForBuy = GetItemByNumber(number);
+
+                if (itemForBuy == null)
+                {
+                    Console.WriteLine($"Ошибка. предмета под номером {number} не существует.");
+                    Console.ReadKey();
+                    return;
+                }
 
                 if (player.Money >= itemForBuy.Price)
                 {
-                    player.GetItem(TakeProduct(itemForBuy, player.TakeMoney(itemForBuy.Price)));
+                    player.GetItem(TakeItem(itemForBuy, player.TakeMoney(itemForBuy.Price)));
+                    _items.Remove(itemForBuy);
                 }
             }
             else
@@ -162,55 +222,11 @@
         }
     }
 
-    class Player
+    class Player : Human
     {
-        private List<Item> _items = new List<Item>();
-
         public Player()
         {
             Money = 10000;
-        }
-
-        public int Money { get; private set; }
-
-        public int TakeMoney(int price)
-        {
-            if (price <= Money)
-            {
-                return Money -= price;
-            }
-
-            return 0;
-        }
-
-        public void GetItem(Item item)
-        {
-            if (item != null)
-            {
-                _items.Add(item);
-            }
-        }
-
-        public void ShowInfo()
-        {
-            Console.Clear();
-            ShowMoney();
-            Console.WriteLine();
-            ShowItems();
-            Console.ReadKey();
-        }
-
-        private void ShowItems()
-        {
-            foreach (var item in _items)
-            {
-                item.ShowInfo();
-            }
-        }
-
-        private void ShowMoney()
-        {
-            Console.WriteLine($"Монеты: {Money}");
         }
     }
 }

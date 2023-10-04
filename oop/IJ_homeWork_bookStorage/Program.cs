54e49187126e3fc3ae7fd85aaafd2f7f4fdc45e4
feaxmutch <feaxmutch@gmail.com>
2023-10-04 15:00:28 +0300

    04.10.2023.15:00

--- oop/IJ_homeWork_bookStorage/Program.cs
+++ oop/IJ_homeWork_bookStorage/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            
+            Storage storage = new();
+
+            storage.ShowMenu();
         }
     }
 
@@ -12,7 +14,52 @@
     {
         private List<Book> _books = new List<Book>();
 
-        public void AddBook()
+        public void ShowMenu()
+        {
+            const ConsoleKey MenuAdd = ConsoleKey.D1;
+            const ConsoleKey MenuRemove = ConsoleKey.D2;
+            const ConsoleKey MenuShowAll = ConsoleKey.D3;
+            const ConsoleKey MenuSearch = ConsoleKey.D4;
+            const ConsoleKey CommandEscape = ConsoleKey.Escape;
+
+            bool isSelecting = true;
+
+            while (isSelecting)
+            {
+                Console.Clear();
+                Console.Write($"{MenuAdd}) Добавить книгу" +
+                            $"\n{MenuRemove}) Удалить книгу" +
+                            $"\n{MenuShowAll}) Отобразить все книги" +
+                            $"\n{MenuSearch}) Найти книгу");
+
+                ConsoleKey userInput = Console.ReadKey(true).Key;
+
+                switch (userInput)
+                {
+                    case CommandEscape:
+                        isSelecting = false;
+                        break;
+
+                    case MenuAdd:
+                        AddBook();
+                        break;
+
+                    case MenuRemove:
+                        RemoveBook();
+                        break;
+
+                    case MenuShowAll:
+                        ShowAllBooks();
+                        break;
+
+                    case MenuSearch:
+                        ShowSearchMenu();
+                        break;
+                }
+            }
+        }
+
+        private void AddBook()
         {
             Console.Clear();
 
@@ -34,27 +81,199 @@
                 Console.ReadKey();
             }
         }
-        
-        private List<Book> SearchByTitle(List<Book> books, string title)
+
+        private void RemoveBook()
+        {
+            int lastParametеr = (int)BookParametеr.Year;
+            List<Book> booksForRemove = new List<Book>(_books);
+
+            for (int i = 0; i <= lastParametеr; i++)
+            {
+                ShowBooks(booksForRemove);
+                Console.Write("Введите ");
+
+                switch (i)
+                {
+                    case (int)BookParametеr.Title:
+                        Console.Write("название ");
+                        break;
+
+                    case (int)BookParametеr.Autor:
+                        Console.Write("автора ");
+                        break;
+
+                    case (int)BookParametеr.Year:
+                        Console.Write("год выпуска ");
+                        break;
+                }
+
+                Console.Write("книги, которую хотите удалить: ");
+
+                string userInput = Console.ReadLine();
+
+                switch (i)
+                {
+                    case (int)BookParametеr.Title:
+                        booksForRemove = Search(_books, BookParametеr.Title, userInput);
+                        break;
+
+                    case (int)BookParametеr.Autor:
+                        booksForRemove = Search(_books, BookParametеr.Autor, userInput);
+                        break;
+
+                    case (int)BookParametеr.Year:
+                        booksForRemove = Search(_books, BookParametеr.Year, userInput);
+                        break;
+                }
+            }
+
+            if (booksForRemove.Count > 0)
+            {
+                _books.Remove(booksForRemove[0]);
+                Console.WriteLine("Книга не найдена");
+            }
+            else
+            {
+                Console.WriteLine("Книга не найдена");
+            }
+        }
+
+        private void ShowAllBooks()
+        {
+            Console.Clear();
+            ShowBooks(_books);
+            Console.ReadKey();
+        }
+
+        private void ShowSearchMenu()
+        {
+            const string CommandTitle = "1";
+            const string CommandAutor = "2";
+            const string CommandYear = "3";
+
+            List<Book> foundedBooks = new List<Book>();
+            bool isSelecting = true;
+            string searchMode = string.Empty;
+
+            while (isSelecting)
+            {
+                Console.Clear();
+                Console.Write("Выберите параметр, по которому будет производится поиск:" +
+                             $"\n{CommandTitle}) По названию" +
+                             $"\n{CommandAutor}) По автору" +
+                             $"\n{CommandYear}) По году выпуска");
+
+                searchMode = Console.ReadKey(true).KeyChar.ToString();
+                isSelecting = false;
+
+                Console.Clear();
+                Console.Write("Введите ");
+
+                switch (searchMode)
+                {
+                    default:
+                        isSelecting = true;
+                        break;
+
+                    case CommandTitle:
+                        Console.Write("название");
+                        break;
+
+                    case CommandAutor:
+                        Console.Write("автора");
+                        break;
+
+                    case CommandYear:
+                        Console.Write("год выпуска");
+                        break;
+                }
+
+                Console.Write(": ");
+            }
+
+            string searchRequest = Console.ReadLine();
+
+            switch (searchMode)
+            {
+                case CommandTitle:
+                    foundedBooks = Search(_books, BookParametеr.Title, searchRequest);
+                    break;
+
+                case CommandAutor:
+                    foundedBooks = Search(_books, BookParametеr.Autor, searchRequest);
+                    break;
+
+                case CommandYear:
+                    foundedBooks = Search(_books, BookParametеr.Year, searchRequest);
+                    break;
+            }
+
+            Console.Clear();
+
+            if (foundedBooks.Count > 0)
+            {
+                Console.WriteLine("Найдены следующие книги");
+                ShowBooks(foundedBooks);
+            }
+            else
+            {
+                Console.WriteLine("Не найдено ни одной книги");
+            }
+
+            Console.ReadKey();
+        }
+
+        private List<Book> Search(List<Book> books, BookParametеr parametеr, string searchRequest)
         {
             List<Book> foundedBooks = new List<Book>();
 
-            for (int i = 0; i < books.Count; i++)
+            foreach (var book in books)
             {
-                if (books[i].Title == title)
+                switch (parametеr)
                 {
-                    foundedBooks.Add(books[i]);
+                    case BookParametеr.Title:
+                        if (book.Title.ToLower().Contains(searchRequest.ToLower()))
+                        {
+                            foundedBooks.Add(book);
+                        }
+                        break;
+
+                    case BookParametеr.Autor:
+                        if (book.Autor.ToLower().Contains(searchRequest.ToLower()))
+                        {
+                            foundedBooks.Add(book);
+                        }
+                        break;
+
+                    case BookParametеr.Year:
+                        if (book.Year == int.Parse(searchRequest))
+                        {
+                            foundedBooks.Add(book);
+                        }
+                        break;
                 }
             }
 
             return foundedBooks;
         }
 
-
+        private void ShowBooks(List<Book> books)
+        {
+            if (books != null)
+            {
+                foreach (var book in books)
+                {
+                    book.ShowInfo();
+                }
+            }
+        }
     }
 
     class Book
     {
+        private string _title;
+        private string _autor;
+
         public Book(string title, string autor, int year)
         {
             Title = title;
@@ -62,15 +281,52 @@
             Year = year;
         }
 
-        public string Title { get; private set; }
+        public string Title
+        {
+            get => _title;
 
-        public string Autor { get; private set; }
+            private set
+            {
+                if (value == string.Empty)
+                {
+                    _title = "Не_указанно";
+                }
+                else
+                {
+                    _title = value;
+                }
+            } 
+        }
+
+        public string Autor 
+        {
+            get => _autor;
+            
+            private set
+            {
+                if (value == string.Empty)
+                {
+                    _autor = "Не_указанно";
+                }
+                else
+                {
+                    _autor = value;
+                }
+            } 
+        }
 
         public int Year { get; private set; }
 
-        public void WriteInfo()
+        public void ShowInfo()
         {
             Console.WriteLine($"имя: {Title} автор: {Autor} год выпуска: {Year}");
         }
+    }
+
+    enum BookParametеr
+    {
+        Title,
+        Autor,
+        Year
     }
 }
---
+++ oop/IJ_homeWork_zoo/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace IJ_homeWork_zoo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Zoo
+    {
+        private List<Aviary> _aviaries = new List<Aviary>();
+    }
+
+    class Aviary 
+    {
+        private List<Animal> _animals = new List<Animal>();
+
+        public List<Animal> Animals { get => new List<Animal>(_animals); }
+    }
+
+    abstract class Animal
+    {
+        private string _name;
+        private Gender _gender;
+
+        public Animal(string name, Gender gender)
+        {
+            _name = name;
+            _gender = gender;
+        }
+
+        public string Name { get => _name; }
+
+        public Gender Gender { get => _gender; }
+
+        public abstract void MakeSound();
+    } 
+
+    class Monkey : Animal
+    {
+        public Monkey(Gender gender) : base("Обезьяна", gender){}
+
+        public override void MakeSound()
+        {
+            Console.Write("");
+        }
+    }
+
+    enum Gender
+    {
+        Male,
+        Female
+    }
+}

3c920ce544bd7df062a4eb84bc652057cb7ec11b
feaxmutch <feaxmutch@gmail.com>
2024-01-09 13:45:04 +0300

    09.01.2024.13:45

--- if_else/IJ_homeWork_exit/Program.cs
+++ if_else/IJ_homeWork_exit/Program.cs
@@ -4,12 +4,12 @@
     {
         static void Main(string[] args)
         {
-            string userInput = "";
+            string userInput = string.Empty;
             string wordForExit = "exit";
             
             while (userInput != wordForExit)
             {
-                userInput = "";
+                userInput = string.Empty;
                 Console.Clear();
                 Console.WriteLine($"Напишите \"{wordForExit}\", для того чтобы закрыть программу");
                 userInput = Console.ReadLine();

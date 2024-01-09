0d1337dd5f973a30f0b3798c6546533fcd8fd9cc
feaxmutch <feaxmutch@gmail.com>
2024-01-09 12:32:32 +0300

    09.01.2024.12:32

--- programming_begins/IJ_homeWorkCS_shop/Program.cs
+++ programming_begins/IJ_homeWorkCS_shop/Program.cs
@@ -6,20 +6,21 @@ namespace IJ_homeWorkCS_shop
         static void Main(string[] args)
         {
             int crystalPrice = 34;
-            int userGold = 0;
             int userCrystals = 0;
-            int buyedCrystals = 0;
+
             Console.Write("Введите начальное количество монет: ");
-            userGold = Convert.ToInt32(Console.ReadLine());
+            int userGold = Convert.ToInt32(Console.ReadLine());
             Console.Clear();
+
             Console.WriteLine($"Монеты: {userGold}\n" +
                               $"Кристалы: {userCrystals}\n");
             Console.WriteLine($"Мы продаём кристалы. \nИх цена составляет {crystalPrice} золотых монет за штуку.");
             Console.WriteLine("Сколько кристалов вы хотите купить?");
-            buyedCrystals = Convert.ToInt32(Console.ReadLine());
+            int buyedCrystals = Convert.ToInt32(Console.ReadLine());
             userGold -= buyedCrystals * crystalPrice;
             userCrystals += buyedCrystals;
             Console.Clear();
+
             Console.WriteLine($"Монеты: {userGold}\n" +
                               $"Кристалы: {userCrystals}\n");
             Console.Write($"Вы купили {buyedCrystals} кристалов.");

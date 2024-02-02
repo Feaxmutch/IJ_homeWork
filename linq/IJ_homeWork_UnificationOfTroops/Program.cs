8743a9ff4fc2a0c942b492455efc9909c4c827e8
feaxmutch <feaxmutch@gmail.com>
2024-02-02 15:44:26 +0300

    01.02.2024.15:44

---
+++ linq/IJ_homeWork_UnificationOfTroops/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace IJ_homeWork_UnificationOfTroops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string unificationСondition = "Б";
+
+            List<Soulder> troop1 = new()
+            {
+                new("Свешников"),
+                new("Киселев"),
+                new("Бобров"),
+                new("Рубцов"),
+                new("Беляков"),
+                new("Ильин"),
+                new("Сидоров"),
+                new("Старостин"),
+                new("Бычков"),
+                new("Антипов")
+            };
+
+            List<Soulder> troop2 = new()
+            {
+                new("Савельев"),
+                new("Лазарев"),
+                new("Калинин"),
+                new("Барсуков"),
+                new("Давыдов"),
+                new("Воронин"),
+                new("Борисов"),
+                new("Ларин"),
+                new("Казаков"),
+                new("Беляев"),
+            };
+
+            var souldersForUnification = troop1.Where(soulder => soulder.LastName.StartsWith(unificationСondition)).ToList();
+
+            foreach (var soulder in souldersForUnification)
+            {
+                troop1.Remove(soulder);
+            }
+            
+            troop2.AddRange(souldersForUnification);
+        }
+    }
+
+    class Soulder
+    {
+        public Soulder(string lastName)
+        {
+            LastName = lastName;
+        }
+
+        public string LastName { get; }
+    }
+}
